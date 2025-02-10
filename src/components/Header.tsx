ff7b7f26a287f1ec5d59c2065876826618374096
Joshua Gonzales <jgonzales.ibarrola@gmail.com>
2025-02-10 22:42:18 +0800

    feat: Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header>
+      <nav className="nav">
+        <a href="/" className="nav-link">
+          <img width={40} height={40} src="/cookie.png" alt="cookie-icon" />
+          <h2>Pookie Bake</h2>
+        </a>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
